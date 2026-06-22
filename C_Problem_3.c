28e25245b86d1e2ffd51e7d8965ff7b90b292ea5
Niranjana <ponniranjana2006@gmail.com>
2026-06-22 23:34:16 +0530

    Create C_Problem_3.c

--- C_Problem_3.c
+++ C_Problem_3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int disp_rsum(int);
+int disp_2digit_ones5(int);
 int main()
 {
     int x,y;
     x=0;
-    y=disp_rsum(x);
+    y=disp_2digit_ones5(x);
     printf("%d",y);
     return 0;
 }
-int disp_rsum(int a)
+int disp_2digit_ones5(int a)
 {
     int y,i;
     y=0;
