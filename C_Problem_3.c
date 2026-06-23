44805c7e82c85f9d4af62ed6746101d50ecde336
Niranjana <ponniranjana2006@gmail.com>
2026-06-23 15:48:26 +0530

    Update C_Problem_3.c

--- C_Problem_3.c
+++ C_Problem_3.c
@@ -3,18 +3,17 @@ int disp_2digit_ones5(int);
 int main()
 {
     int x,y;
-    x=0;
     y=disp_2digit_ones5(x);
     printf("%d",y);
     return 0;
 }
-int disp_2digit_ones5(int a)
+int disp_2digit_ones5(int x)
 {
-    int y,i;
-    y=0;
+    int sum,i;
+    sum=0;
     for(i=15;i<=95;i=i+10)
     {
-        y=y+i;
+        sum=sum+i;
     }
-    return y;
+    return sum;
 }
