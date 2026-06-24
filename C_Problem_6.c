4ce96b112b8ada5937ebe38e52aad97cc755c4fd
Niranjana <ponniranjana2006@gmail.com>
2026-06-24 23:42:49 +0530

    Update C_Problem_6.c

--- C_Problem_6.c
+++ C_Problem_6.c
@@ -9,15 +9,15 @@ int main()
     printf("%d",y);
     return 0;
 }
-int disp_sum_all_digits(int a)
+int disp_sum_all_digits(int x)
 {
     int sum,rem;
     sum=0;
-    while(a>0)
+    while(x>0)
     {
-        rem=a%10;
+        rem=x%10;
         sum=sum+rem;
-        a=a/10;
+        x=x/10;
     }
     return sum;
 }
